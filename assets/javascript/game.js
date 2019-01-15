4324fed9940daadd67eb35694130029353667296
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-01-15 12:16:10 -0800

    Selection of enemy done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,17 @@
 var playerChosen = false;
+var enemyChosen = false;
 var yourPlayer = "";
 var enemiesTochoose = "";
 var enemy = "";
 
 $(".character").on("click", function(){
-	var player = $(this).html();
 	
+	var player = $(this).html();
+
 	if (playerChosen == true) {
 		return null;
 	}
-
+	
 	playerChosen = true;
 	$(this).detach();
 	$(".characterDiv").append(player);
@@ -18,4 +20,19 @@ $(".character").on("click", function(){
 
 function appendEnemies(){
 	$(".enemiesDiv").append($(".playersToChoose"));
+	selectEnemy();
+}
+
+function selectEnemy(){
+	$(".character").on("click", function(){
+		
+		var enemy = $(this).html();
+
+		if (enemyChosen == true) {
+		return null;
+	}
+		enemyChosen = true;
+		$(this).detach();
+		$(".defenderDiv").append(enemy);
+	});
 }
