b3f50ad87850b329aa0c0a2d611eb4708d6d4c48
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-01-15 11:15:27 -0800

    Append enemies done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,21 @@
+var playerChosen = false;
+var yourPlayer = "";
+var enemiesTochoose = "";
+var enemy = "";
+
 $(".character").on("click", function(){
 	var player = $(this).html();
+	
+	if (playerChosen == true) {
+		return null;
+	}
+
+	playerChosen = true;
 	$(this).detach();
 	$(".characterDiv").append(player);
-})
+	appendEnemies();
+});
+
+function appendEnemies(){
+	$(".enemiesDiv").append($(".playersToChoose"));
+}
